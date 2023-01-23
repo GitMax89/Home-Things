8f1854201f77318a8b20e142fcc121e6eca5eb34
m.tavernese <max.tvn89@gmail.com>
2023-01-23 22:26:49 +0100

    (+) add: remove item, interactive item

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ let div = document.getElementById('spesa-div');
 let title = document.getElementById('title');
 let input = document.getElementById('text-input')
 let button = document.getElementById('spesa-button');
+
 // add eventlistner
 button.addEventListener('click', addItem)
 // document.addEventListener('DOMContentLoaded', displayStorage);
 
 
+// crea il template dell'elemento HTML in lista e ne verifica il contenuto
 function createItemsList(value){
     let p = document.createElement('p')
+    p.setAttribute('id', `${value}`)
     p.innerHTML = value
+    p.addEventListener('click', function(){
+        p.style.textDecoration = 'line-through'
+    })
+    p.addEventListener('dblclick', function(){
+        removeItems(value)
+    })
     if(value === ''){
         p.innerHTML = 'Non hai inserito alcun elemento, perfavore inserisci un elemento'
         p.style.color = 'red'
@@ -19,37 +28,43 @@ function createItemsList(value){
         setTimeout(function(){
             p.remove();
         }, 3000)
-    } else {
+    } 
+    // else if(displayStorage().then(json => json) == checkItemsInList(value)){
+    //     checkItemsInList(value)
+    // }
+    else {
         div.appendChild(p)
     }
     return p
 }
 
-// function add item on list
+// gestisce gli elementi in lista
 function addItem(){
     let value = input.value
     if (value === ''){
         createItemsList(value)
-    } else {
-        writeStorage()
-        
     }
+    else {
+        checkItemsInList(value)
+        createItemsList(value)
+        writeStorage(value)
+    }
+
 }
 
 
-
+// mostra gli elementi in lista
 async function displayStorage(){
-    const apiURL = fetch('http://vimaxnas.ddns.net:3000/lista')
+    const apiURL = fetch('http://192.168.178.105:3000/lista')
     // gestisci il successo
-    .then(response => response.json())  // converti a json
+    .then(response => response.json())  // converto in json
     .then(json => {
         value = json
         return Object.values(value)
-    })    // stampa i dati sulla console
+    })    // restituisco i dati della response
     .catch(err => console.log('Request Failed', err)); 
     return apiURL
-}
-// console.log(createItemsList(displayStorage()))
+} // iterazione sugli elementi che verranno mostrati in lista
 displayStorage().then(json => {
     for (i=0;i<json.length;i++){
         let v = json[`${i}`].product
@@ -57,12 +72,15 @@ displayStorage().then(json => {
     }
 })
 
-function writeStorage(){
+
+
+// scrivo gli elementi provenienti dal text-input su file JSON
+function writeStorage(value){
     let data = {
-        product: `${input.value}`
+        product: `${value}`
     }
 
-    fetch('http://vimaxnas.ddns.net:3000/lista', {
+    fetch('http://192.168.178.105:3000/lista', {
         method: "POST",
         body: JSON.stringify(data),
         headers: {"Content-type": "application/json; charset=UTF-8"}
@@ -70,3 +88,36 @@ function writeStorage(){
     .then(response => response.json())
     .then(response => console.log(response.json()));
 }
+
+// verifico i duplicati in lsta
+function checkItemsInList(value){
+    let x = []
+    displayStorage().then(json => {
+        for (i=0;i<json.length;i++){
+            x.push(json[`${i}`].product)
+            if (value == x[`${i}`]){
+                alert(`${value} è già presente in lista`)
+            }
+        }
+    })
+}
+
+// rimuovo i singoli elementi
+// let c = 'pera'
+function removeItems(value){
+    displayStorage().then(json => {
+        for (i=0;i<json.length;i++){
+            a = json[`${i}`]
+            if (value == a.product){
+                let id = a.id
+                fetch('http://192.168.178.105:3000/lista/' + id, {
+                    method: 'DELETE',
+                })
+                .then(res => res.json()) // or res.json()
+                .then(res => console.log(res))
+            }
+        } 
+    })
+}
+// removeItems(c)
+
