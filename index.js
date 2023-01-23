822b3cfc1d7eecf4a42363a37ddc22646789e0b2
m.tavernese <max.tvn89@gmail.com>
2023-01-23 12:34:34 +0100

    (+) add JSon Storage

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let input = document.getElementById('text-input')
 let button = document.getElementById('spesa-button');
 // add eventlistner
 button.addEventListener('click', addItem)
-document.addEventListener('DOMContentLoaded', displayStorage);
+// document.addEventListener('DOMContentLoaded', displayStorage);
 
 
 function createItemsList(value){
@@ -15,12 +15,13 @@ function createItemsList(value){
     if(value === ''){
         p.innerHTML = 'Non hai inserito alcun elemento, perfavore inserisci un elemento'
         p.style.color = 'red'
-        form.appendChild(p)
+        div.appendChild(p)
         setTimeout(function(){
             p.remove();
         }, 3000)
+    } else {
+        div.appendChild(p)
     }
-    div.appendChild(p)
     return p
 }
 
@@ -30,41 +31,42 @@ function addItem(){
     if (value === ''){
         createItemsList(value)
     } else {
-        createItemsList(value)
-        updateStorage(value)
-    }
-}
-
-// update storage from element list
-function updateStorage(value){
-    
-    let itemList = localStorage.getItem('itemList') ? JSON.parse(localStorage.getItem('itemList')) : [];
-
-    itemList.push(value);
-    localStorage.setItem('itemList', JSON.stringify(itemList));
-    console.log(itemList)
-}
-
-
-// display item in local storage
-function displayStorage(){
-    let exists = localStorage.getItem('itemList');
-    
-    if(exists){
-        let storageItems = JSON.parse(localStorage.getItem('itemList'));
-        storageItems.forEach(function(element){
-            createItemsList(element);
-        })
+        writeStorage()
+        
     }
 }
 
 
 
+async function displayStorage(){
+    const apiURL = fetch('http://192.168.178.105:3000/lista')
+    // gestisci il successo
+    .then(response => response.json())  // converti a json
+    .then(json => {
+        value = json
+        return Object.values(value)
+    })    // stampa i dati sulla console
+    .catch(err => console.log('Request Failed', err)); 
+    return apiURL
+}
+// console.log(createItemsList(displayStorage()))
+displayStorage().then(json => {
+    for (i=0;i<json.length;i++){
+        let v = json[`${i}`].product
+        createItemsList(v)
+    }
+})
 
+function writeStorage(){
+    let data = {
+        product: `${input.value}`
+    }
 
-
-
-
-    
-
-
+    fetch('http://192.168.178.105:3000/lista', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(response => console.log(response.json()));
+}
